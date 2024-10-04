53d12362822138b154d8f9dc4d7756c3296a9b16
Alexandre Dulaunoy <a@foo.be>
2024-10-04 17:09:25 +0200

    new: [onion-lookup] skeleton added

---
+++ README.md
@@ -0,0 +1,11 @@
+# onion-lookup
+
+Software back-end and services for checking the existence of Tor hidden services and retrieving their associated metadata. onion-lookup relies on an AIL instance to obtain the metadata.
+
+# License
+
+This software is licensed under GNU Affero General Public License version 3.
+
+- Copyright (C) 2024 CIRCL - Computer Incident Response Center Luxembourg
+- Copyright (C) 2024 Alexandre Dulaunoy
+- Copyright (C) 2024 AIL Project
---
+++ app.py
@@ -0,0 +1,61 @@
+from starlette.applications import Starlette
+from starlette.exceptions import HTTPException
+from starlette.requests import Request
+from starlette.staticfiles import StaticFiles
+from starlette.routing import Route, Mount
+from starlette.templating import Jinja2Templates
+import uvicorn
+
+
+templates = Jinja2Templates(directory='templates')
+
+
+async def homepage(request):
+    template = "index.html"
+    context = {"request": request}
+    if 'lookup' in request.query_params:
+        print(request.query_params['lookup'])
+    return templates.TemplateResponse(template, context)
+
+
+async def error(request):
+    """
+    Generic catch-call error 
+    """
+    raise RuntimeError("Oh no")
+
+
+async def not_found(request: Request, exc: HTTPException):
+    """
+    Return an HTTP 404 page.
+    """
+    template = "404.html"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context, status_code=404)
+
+
+async def server_error(request: Request, exc: HTTPException):
+    """
+    Return an HTTP 500 page.
+    """
+    template = "500.html"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context, status_code=500)
+
+routes = [
+    Route('/', homepage),
+    Route('/error', error),
+    Mount('/static', app=StaticFiles(directory='statics'), name='static')
+]
+
+exception_handlers = {
+    404: not_found,
+    500: server_error
+}
+
+app = Starlette(debug=True, routes=routes, exception_handlers=exception_handlers)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8000)
+    
