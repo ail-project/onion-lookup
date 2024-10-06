71fe9fecc371573f0d356153287bdc01a4dcb8fd
Alexandre Dulaunoy <a@foo.be>
2024-10-06 16:28:48 +0200

    chg: [API] OpenAPI added + fix for multiple slashes in the url

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from starlette.requests import Request
 from starlette.staticfiles import StaticFiles
 from starlette.routing import Route, Mount
 from starlette.templating import Jinja2Templates
+from starlette.responses import JSONResponse, RedirectResponse
+from starlette.datastructures import URL
+from starlette.types import ASGIApp, Receive, Scope, Send
+repeated_quotes = re.compile(r'//+') # handling multiple // in url
 import sys
 import uvicorn
 import valkey
@@ -22,9 +26,32 @@ ail_apikey = config['DEFAULT']['ail_apikey']
 valkey_host = config['DEFAULT']['valkey_host']
 valkey_port = config['DEFAULT']['valkey_port']
 cache = valkey.Valkey(host=valkey_host, port=valkey_port, db=0)
+# API definition
+from apiman.starlette import Apiman
+apiman = Apiman(template="openapi.yml")
 
 if not cache.ping():
     sys.exit("Valkey server not reachable")
+
+
+class HttpUrlRedirectMiddleware:
+  """
+  This http middleware redirects urls with repeated slashes to the cleaned up
+  versions of the urls - from GitHub issue from Starlette project
+  """
+
+  def __init__(self, app: ASGIApp) -> None:
+    self.app = app
+
+  async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+
+    if scope["type"] == "http" and repeated_quotes.search(URL(scope=scope).path):
+      url = URL(scope=scope)
+      url = url.replace(path=repeated_quotes.sub('/', url.path))
+      response = RedirectResponse(url, status_code=307)
+      await response(scope, receive, send)
+    else:
+      await self.app(scope, receive, send)
 
 def check_onion(onion=None):
     # We only support onion_v3 and automatically append dot onion if missing
@@ -46,9 +73,8 @@ def query_onion(onion=None):
     r = requests.get(f'{ail_url}/api/v1/lookup/onion/{onion}', headers=headers, verify=False)
     if r.status_code != 200:
         return False
-    print(r.status_code)
-    print(r.json())
     cache.set(keycache, r.text, ex=3600)
+    return json.loads(cache.get(keycache))
 
 async def homepage(request):
     template = "index.html"
@@ -65,6 +91,32 @@ async def homepage(request):
 
     return templates.TemplateResponse(template, context)
 
+@apiman.from_yaml(
+    """
+    summary: lookup api
+    tags:
+    - lookup
+    parameters:
+    - name: onion 
+      in: path 
+      required: True
+    responses:
+      "200":
+        description: OK
+    """)
+async def lookup(request):
+    onion = check_onion(onion=request.path_params['onion'].lower())
+    keycache = f'onion-lookup:{onion}'
+    if onion:
+        onion_response = check_onion(onion=onion)
+        if onion_response is not False:
+            onion_meta = query_onion(onion=onion)
+            if onion_meta is not False:
+                return JSONResponse(onion_meta)
+        else:
+            return JSONResponse({"error": "Incorrect onion format"})
+
+    return JSONResponse({})
 
 async def error(request):
     """
@@ -92,8 +144,10 @@ async def server_error(request: Request, exc: HTTPException):
 
 routes = [
     Route('/', homepage),
+    Route('/api/lookup/{onion}', lookup, methods=['GET']),
     Route('/error', error),
     Mount('/static', app=StaticFiles(directory='statics'), name='static')
+
 ]
 
 exception_handlers = {
@@ -102,7 +156,8 @@ exception_handlers = {
 }
 
 app = Starlette(debug=True, routes=routes, exception_handlers=exception_handlers)
-
+app.add_middleware(HttpUrlRedirectMiddleware)
+apiman.init_app(app)
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=8000)
